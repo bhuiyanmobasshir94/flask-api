9eb2a3a7e8a220a655e183b857f8260bff28e1ad
bhuiyanmobasshir94 <mobasshirbhuiyan.shagor@gmail.com>
2019-04-17 15:56:55 +0600

    Second commit from flask-api

--- server.py
+++ server.py
@@ -1,14 +1,30 @@
+    
+"""
+Main module of the server file
+"""
+
+# 3rd party moudles
 from flask import render_template
 import connexion
 
-app = connexion.App(__name__, specification_dir='./')
 
-app.add_api('swagger.yml')
+# Create the application instance
+app = connexion.App(__name__, specification_dir="./")
 
-@app.route('/')
+# Read the swagger.yml file to configure the endpoints
+app.add_api("swagger.yml")
+
+
+# create a URL route in our application for "/"
+@app.route("/")
 def home():
-    return render_template('home.html')
+    """
+    This function just responds to the browser URL
+    localhost:5000/
+    :return:        the rendered template "home.html"
+    """
+    return render_template("home.html")
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port= 5000, debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
